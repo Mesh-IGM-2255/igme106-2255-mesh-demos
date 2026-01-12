884b08ad986f9fb9a3913a56e3444369dd167234
Erika Mesh <esmvcs@rit.edu>
2026-01-12 15:18:13 -0500

    Mon 1/12 - Demo adding a new PE

---
+++ PEs/PE_HelloWorld/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace PE_HelloWorld
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello 106 folks!");
+        }
+    }
+}
