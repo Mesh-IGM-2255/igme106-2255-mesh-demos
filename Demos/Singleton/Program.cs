fc35a41ab9f0188fee6de163e99a88a3b1849c9d
Erika Mesh <esmvcs@rit.edu>
2026-01-23 15:47:04 -0500

    Fri 1/23 - singleton demo

---
+++ Demos/Singleton/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Singleton
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numTests = 100;
+
+            for(int i = 0; i<numTests; i++)
+            {
+                Random rng = new Random(DateTime.Now.Millisecond);
+                Console.Write("*");
+                Console.Write(rng.Next(1, 10000));
+
+                SingleRNG rngS = SingleRNG.Instance;
+                Console.WriteLine("\t\t"+rngS.Next(1, 10000));
+            }
+        }
+    }
+}
---
+++ Demos/Singleton/SingleRNG.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    internal class SingleRNG
+    {
+
+        private static SingleRNG self;
+
+        private Random rng;
+
+        private SingleRNG()
+        {
+            rng = new Random(DateTime.Now.Millisecond);
+            Console.Write("*");
+        }
+
+        public static SingleRNG Instance
+        {
+            get
+            {
+                if(self == null)
+                {
+                    self = new SingleRNG();
+                }
+                return self;
+            }
+        }
+
+        public int Next(int min, int max)
+        {
+            return rng.Next(min, max);
+        }
+
+
+    }
+}
