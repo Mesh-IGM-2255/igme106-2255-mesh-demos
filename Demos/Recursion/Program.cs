9c5b4f543c321bb746699af9f3d7cd95f7760640
Erika Mesh <esmvcs@rit.edu>
2026-01-28 14:44:17 -0500

    Wed 1/28 - Recursion

---
+++ Demos/Recursion/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace Recursion
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Factorial - recursive: " + Factorial(5));
+            Console.WriteLine("Factorial - iterative: " + Factorial_iterative(5));
+        }
+
+
+
+        static int Factorial(int n)
+        {
+            // base case
+            if(n<=1)
+            {
+                return 1;
+            }
+            //         recursive call -- WITH state change towards base case 
+            return n * Factorial(n-1);
+        }
+
+        static int Factorial_iterative(int n)
+        {
+            int result = 1;
+//            for(int i=1; i<=n; i++)
+            while(n>0)
+            {
+                result *= n;
+                n--;
+            }
+            return result;
+        }
+    }
+}
