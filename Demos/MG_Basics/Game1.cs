3ff23c1f1513717d2d7192d11d316c284ea7e71b
Erika Mesh <esmvcs@rit.edu>
2026-03-02 15:31:23 -0500

    Mon 3/2 - MonoGame input & text

--- Demos/MG_Basics/Game1.cs
+++ Demos/MG_Basics/Game1.cs
@@ -13,6 +13,12 @@ namespace MG_Basics
         private const float pandaScale = 0.1f;
         private const int DesiredWidth = 1200;
         private const int DesiredHeight = 900;
+        private Rectangle playerLoc;
+        private int speed = 2;
+        KeyboardState prevKbState;
+
+        private SpriteFont uiFont;
+        private SpriteFont debugFont;
 
         public Game1()
         {
@@ -37,10 +43,13 @@ namespace MG_Basics
 
             // TODO: use this.Content to load your game content here
             pandaImg = Content.Load<Texture2D>("panda");
+            uiFont = Content.Load<SpriteFont>("uiFont");
+            debugFont = Content.Load<SpriteFont>("debugFont");
 
             pandaLoc = new Rectangle(10, 10, 
                 (int)(pandaImg.Width*pandaScale),
                 (int)(pandaImg.Height*pandaScale));
+            playerLoc = new Rectangle(10, 10, DesiredWidth / 8, DesiredHeight / 8);
         }
 
         protected override void Update(GameTime gameTime)
@@ -52,6 +61,41 @@ namespace MG_Basics
             // TODO: Add your update logic here
             pandaLoc.X++;
             pandaLoc.Y++;
+
+            KeyboardState currentkbState = Keyboard.GetState(); // GOOD
+
+            // BAD
+            // KeyboardState defaultKbState = new KeyboardState();
+
+            if(currentkbState.IsKeyUp(Keys.Space) && prevKbState.IsKeyDown(Keys.Space))
+            {
+                //playerLoc.X++;
+                //playerLoc.Y++;
+                speed++;
+            }
+
+
+            if(currentkbState.IsKeyDown(Keys.W))
+            {
+                playerLoc.Y -= speed;
+            }
+
+            if (currentkbState.IsKeyDown(Keys.A))
+            {
+                playerLoc.X -= speed;
+            }
+
+            if (currentkbState.IsKeyDown(Keys.S))
+            {
+                playerLoc.Y += speed;
+            }
+
+            if (currentkbState.IsKeyDown(Keys.D))
+            {
+                playerLoc.X += speed;
+            }
+
+            prevKbState = currentkbState;
 
             base.Update(gameTime);
         }
@@ -68,8 +112,16 @@ namespace MG_Basics
             _spriteBatch.Draw(pandaImg, pandaLoc, Color.White);
 
             _spriteBatch.Draw(pandaImg, 
-                new Rectangle(10, 10, DesiredWidth/8, DesiredHeight/8), 
+                playerLoc, 
                 Color.Magenta);
+
+            // UI
+            // draw text: text string, font info, position, color
+            _spriteBatch.DrawString(uiFont, "Controls: WASD\nGo explore!", new Vector2(20, 200), Color.DarkGreen);
+            _spriteBatch.DrawString(
+                debugFont,
+                playerLoc.X.ToString() + ", " + playerLoc.Y.ToString() + "\nspeed: " + speed.ToString(),
+                new Vector2(800, 20), Color.DarkGreen);
 
 
             _spriteBatch.End();
